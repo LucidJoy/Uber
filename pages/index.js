33469fe1557dc5f889ba852d32d3bfd634f18289
LucidJoy <jyotirmoykarmakar02@gmail.com>
2022-03-29 22:02:09 +0530

    project setup & tailwind styled components installation

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import tw from "tailwind-styled-components";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.map}>Map</div>
+      <div className={styles.start}>Start</div>
+    </div>
+  );
+}
