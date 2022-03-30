2a372facff9883852ebb912bd39f3c2133bf1e48
LucidJoy <jyotirmoykarmakar02@gmail.com>
2022-03-30 12:54:01 +0530

    Header component built

---
+++ pages/components/Map.jsx
@@ -0,0 +1,25 @@
+import React, { useEffect } from "react";
+import tw from "tailwind-styled-components";
+import mapboxgl from "!mapbox-gl";
+
+mapboxgl.accessToken =
+  "pk.eyJ1IjoibHVjaWRqb3kiLCJhIjoiY2wxY2VvZjFoMDZ4cDNranBocnZ3eTk5MiJ9.9I40XH_HPbhN4tHCDryq4g";
+
+const Map = () => {
+  useEffect(() => {
+    const map = new mapboxgl.Map({
+      container: "map",
+      style: "mapbox://styles/drakosi/ckvcwq3rwdw4314o3i2ho8tph",
+      center: [-99.29011, 39.39172],
+      zoom: 3,
+    });
+  });
+
+  return <Wrapper id="map">Map</Wrapper>;
+};
+
+export default Map;
+
+const Wrapper = tw.div`
+  flex flex-col flex-1
+`;
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,54 @@
+import { useEffect } from "react";
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import tw from "tailwind-styled-components";
 
+import Map from "./components/Map";
+
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <div className={styles.map}>Map</div>
-      <div className={styles.start}>Start</div>
-    </div>
+    <Wrapper>
+      <Map />
+      <ActionItem>
+        <Header>
+          <UberLogo src="https://i.ibb.co/84stgjq/uber-technologies-new-20218114.jpg" />
+
+          <Profile>
+            <Name>Joy</Name>
+            <UserImage src="https://lh3.googleusercontent.com/a-/AOh14GjFZeddOQcd2taGkjQDIoKkB0ij1lRZKd4-z6PBWQ=s432-p-rw-no" />
+          </Profile>
+        </Header>
+      </ActionItem>
+    </Wrapper>
   );
 }
+
+const Wrapper = tw.div`
+  flex flex-col h-screen 
+`;
+
+const ActionItem = tw.div`
+   flex-1 p-4
+`;
+
+const Header = tw.div`
+flex justify-between items-center
+`;
+
+const UberLogo = tw.img`
+  h-28
+`;
+
+const Profile = tw.div`
+  flex items-center
+
+`;
+
+const Name = tw.div`
+mr-4 w-20 text-sm font-semibold
+`;
+
+const UserImage = tw.img`
+  h-12 w-12 rounded-full border border-gray-300 p-px
+`;
