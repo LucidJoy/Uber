f09179bfd966f8486579377ee1b0577be6e17e66
LucidJoy <jyotirmoykarmakar02@gmail.com>
2022-03-30 22:19:59 +0530

    map component built

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import tw from "tailwind-styled-components";
+import Link from "next/link";
 
 import Map from "./components/Map";
 
@@ -19,6 +20,25 @@ export default function Home() {
             <UserImage src="https://lh3.googleusercontent.com/a-/AOh14GjFZeddOQcd2taGkjQDIoKkB0ij1lRZKd4-z6PBWQ=s432-p-rw-no" />
           </Profile>
         </Header>
+
+        <ActionButtons>
+          <Link href="/search">
+            <ActionButton>
+              <ActionButtonImage src="https://i.ibb.co/cyvcpfF/uberx.png" />
+              Ride
+            </ActionButton>
+          </Link>
+          <ActionButton>
+            <ActionButtonImage src="https://i.ibb.co/n776JLm/bike.png" />
+            Wheels
+          </ActionButton>
+          <ActionButton>
+            <ActionButtonImage src="https://i.ibb.co/5RjchBg/uberschedule.png" />
+            Reserve
+          </ActionButton>
+        </ActionButtons>
+
+        <InputButton>Where to? </InputButton>
       </ActionItem>
     </Wrapper>
   );
@@ -51,4 +71,20 @@ mr-4 w-20 text-sm font-semibold
 
 const UserImage = tw.img`
   h-12 w-12 rounded-full border border-gray-300 p-px
+`;
+
+const ActionButtons = tw.div`
+  flex 
+`;
+
+const ActionButton = tw.div`
+  bg-gray-200 flex-1 h-32 m-2 flex flex-col items-center justify-center rounded-lg transform hover:scale-105 transition text-lg
+`;
+
+const ActionButtonImage = tw.img`
+  h-3/5
+`;
+
+const InputButton = tw.div`
+  bg-gray-200 h-20 text-lg p-4 flex items-center mt-8 rounded-lg
 `;
